06c575a86e4f1b6d7fc53d356f4a87e06236b54c
shobhit7390 <vms6402@gmail.com>
2023-08-20 11:43:37 +0530

    Values and Variables

--- Fundamental_1/script.js
+++ Fundamental_1/script.js
@@ -1,5 +1,32 @@
 
 let js='amazing';
-if(js === 'amazing') alert("JavaScript is FUN!");
-
+// if(js === 'amazing') alert("JavaScript is FUN!");
 console.log(40+8+23-10);
+
+console.log("Jonas");
+console.log(23);
+
+// Values and Variables
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
