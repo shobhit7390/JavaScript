76691e6b644e038de7db8ee42b39132dbf43c41d
shobhit7390 <vms6402@gmail.com>
2023-08-21 10:44:45 +0530

    Data Types

--- Fundamental_1/script.js
+++ Fundamental_1/script.js
@@ -1,12 +1,14 @@
-
+/*
 let js='amazing';
-// if(js === 'amazing') alert("JavaScript is FUN!");
 console.log(40+8+23-10);
 
 console.log("Jonas");
 console.log(23);
+*/
 
 // Values and Variables
+
+/*
 console.log("Jonas");
 console.log(23);
 
@@ -15,8 +17,11 @@ let firstName = "Matilda";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+*/
 
 // Variable name conventions
+
+/*
 let jonas_matilda = "JM";
 let $function = 27;
 
@@ -30,3 +35,28 @@ let job1 = "programmer";
 let job2 = "teacher";
 
 console.log(myFirstJob);
+*/
+
+// Data Types
+
+let javascriptIsFun= true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!'
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year=2001;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
