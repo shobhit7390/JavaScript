c256fd8bd3da0b18d76e25421fc0354764412ca0
shobhit7390 <vms6402@gmail.com>
2023-08-23 19:29:10 +0530

    Strings and Literals

--- Fundamental_1/script.js
+++ Fundamental_1/script.js
@@ -73,7 +73,7 @@ job='teacher';
 
 lastName='Yadav';
 console.log(lastName);
-*/
+
 
 // Math Operators
 
@@ -102,9 +102,32 @@ x--;
 
 console.log(x);
 
-// Comparison Operators
+// Comparison Operators : >, <, >=, <=, ==, etc
 
-// >, <, >=, <=, ==, etc
+*/
+
+// Strings and Template Literals
+
+const firstName='Jonas';
+const job='teacher';
+const birthYear=1991;
+const currentYear=2037;
+
+const jonas="I'm "+firstName+', a '+(currentYear-birthYear)+' years old '+job+'!';
+
+console.log(jonas);
+const jonasNew=`I'm ${firstName}, a ${currentYear-birthYear} years old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String 
+multiple 
+lines`);
 
 
 
