cd8a025a3c3920ddbc5bf99a1716346b7bf049d8
shobhit7390 <vms6402@gmail.com>
2023-08-23 09:06:52 +0530

    Assignments script

---
+++ Assignments/assign.js
@@ -0,0 +1,24 @@
+
+// Assignment 1 :
+
+let country = 'India';
+let continent = 'Asia';
+let population = '130';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+// Assignment 2 :
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+
+
