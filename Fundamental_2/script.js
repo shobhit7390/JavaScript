e30a28013028fee4384474f70a909f6f67682a64
shobhit7390 <vms6402@gmail.com>
2023-12-02 21:22:43 +0530

    Functions

--- Fundamental_2/script.js
+++ Fundamental_2/script.js
@@ -1,4 +1,5 @@
 'use strict';
+/*
 
 let hasDriversLicense=false;
 const passTest=true;
@@ -8,4 +9,30 @@ if(hasDriversLicense) console.log("I can drive :D");
 
 // const interface='Audio';
 // const private=534;
-// const if=23;
+// const if=23; 
+
+*/
+
+// Functions
+
+function logger(){
+    console.log('My name is Shobhit Yadav');
+}
+
+logger(11);
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+    // console.log(apples, oranges);
+    const juice=`Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice=fruitProcessor(4,0);
+console.log(appleJuice);
+
+console.log(fruitProcessor(4,0));
+
+const appleOrangeJuice=fruitProcessor(2,5);
+console.log(appleOrangeJuice);
