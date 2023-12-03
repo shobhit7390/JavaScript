b1ee2252d53bd4b0eac6a97f37f0f4fb04ca0fe0
shobhit7390 <vms6402@gmail.com>
2023-12-03 20:50:24 +0530

    Coding Challenge 5

--- Coding_Challenges/coding.js
+++ Coding_Challenges/coding.js
@@ -150,7 +150,63 @@ Value X is between 50 and 300, if it's >= 50 && <= 300 😉
 
 */
 
+// Solution:
+
+/*
+
 const bill=275;
 const tip=bill>=50 && bill<=300 ? bill*0.15 : bill*0.2;
 
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill+tip}.`);
+
+*/
+
+
+
+// Coding Challenge #5
+
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+A team only wins if it has at least double the average score of the other team.
+Otherwise, no team wins!
+
+Your tasks:
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above.
+Example: "Koalas win (30 vs. 13)"
+4. Use the 'checkWinner' function to determine the winner for both Data 1 and Data 2
+5. Ignore draws this time
+
+Test data:
+
+Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+*/
+
+// Solution:
+
+
+const calcAverage=(a,b,c) => (a+b+c)/3;
+
+let score_D=calcAverage(44,23,71);
+let score_K=calcAverage(65,54,49);
+console.log(score_D,score_K);
+
+const checkWinner = function(avg_D,avg_K){
+    if(avg_D >= 2*avg_K){
+        console.log(`Dolphins wins the trophy (${avg_D} vs ${avg_K})`);
+    } else if(avg_K >= 2*avg_D) {
+        console.log(`Koalas wins the trophy (${avg_K} vs ${avg_D})`);
+    } else {
+        console.log("No team wins..!");
+    }
+}
+
+checkWinner(score_D,score_K);
+checkWinner(66,488);
+
+
+
