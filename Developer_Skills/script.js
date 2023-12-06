010b0de6309d078831e4c7d602ce624c8a591ebe
shobhit7390 <vms6402@gmail.com>
2023-12-06 14:04:08 +0530

    Setting up prettier and vscode

---
+++ Developer_Skills/script.js
@@ -0,0 +1,10 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+const x = '23';
+
+if (x === 23) console.log(x);
+
+const calcAge = birthYear => 2023 - birthYear;
+
+console.log();
