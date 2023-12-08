1cce339258d79d7b7a179c8debdc1029e18439a4
shobhit7390 <vms6402@gmail.com>
2023-12-08 11:18:25 +0530

    Manipulating CSS styles

--- 5-Guess-Number-Project/script.js
+++ 5-Guess-Number-Project/script.js
@@ -22,10 +22,17 @@ document.querySelector('.check').addEventListener('click', function () {
 
   console.log(guess, typeof guess);
 
+  // No input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high!';
@@ -35,6 +42,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too low!';
