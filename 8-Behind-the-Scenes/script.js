5047b5f6ac40cdd6550e81e80a1db5d39a383ddb
shobhit7390 <vms6402@gmail.com>
2023-12-10 20:45:40 +0530

    Hoisting and TDZ

--- 8-Behind-the-Scenes/script.js
+++ 8-Behind-the-Scenes/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/*
+
 function calcAge(birthYear) {
   const age = 2023 - birthYear;
 
@@ -34,3 +36,50 @@ const firstName = "Shobhit";
 calcAge(2001);
 
 // console.log(age);
+
+*/
+
+// Hoisting and TDZ
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "Shobhit";
+let job = "SWE";
+const year = 2001;
+
+// Functions
+console.log(addDecl(4, 5));
+// console.log(addExpr(4, 5));
+console.log(addArrow);
+// console.log(addArrow(4, 5));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example:
+// console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("All products deleted");
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
