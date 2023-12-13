19bbc95850547563202723592fbbdbe740cd0e8c
shobhit7390 <vms6402@gmail.com>
2023-12-13 13:19:18 +0530

    Logical Assignment Operator

--- 9-Data-Structures-Operators/script.js
+++ 9-Data-Structures-Operators/script.js
@@ -54,9 +54,45 @@ const restaurant = {
   },
 };
 
+///////////////////////////////
+// Logical Assignment Operator
+
+const rest1 = {
+  name: "Capri",
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: "La Piazza",
+  owner: "Giovanni Rossi",
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// Nullsish assignment operator
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && "<ANONYMOUS>";
+// rest2.owner = rest2.owner && "<ANONYMOUS>";
+
+rest1.owner &&= "<ANONYMOUS>";
+rest2.owner &&= "<ANONYMOUS>";
+
+console.log(rest1);
+console.log(rest2);
+
 /////////////////////////////
 // Nully Coalescing operator
 
+/*
 restaurant.numGuests = 0;
 
 const guests = restaurant.numGuests || 10;
@@ -65,6 +101,7 @@ console.log(guests);
 // Nullish : null and undefined(NOT 0 or '')
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
+*/
 
 ///////////////////////////////
 // Short Circuiting(|| and &&)
