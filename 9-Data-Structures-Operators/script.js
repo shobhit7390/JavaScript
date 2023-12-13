addbccf64e476e6952efedfc2ae9c241d9f68940
shobhit7390 <vms6402@gmail.com>
2023-12-13 17:56:57 +0530

    Optional Chaining

--- 9-Data-Structures-Operators/script.js
+++ 9-Data-Structures-Operators/script.js
@@ -53,6 +53,28 @@ const restaurant = {
   },
 };
 
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// With Optional Chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example:
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? "closed";
+  console.log(`On ${day}, weopen at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
+console.log(restaurant.orderRissotto?.(0, 1) ?? "Method does not exist");
+
+// Arrays
+const users = [{ name: "Jonas", email: "hello@jonas,io" }];
+console.log(users[0]?.name ?? "User array empty");
+
 ///////////////////
 // The for of loop
 /*
