1796857d7807ace8d686edea68b41324e7ca6268
shobhit7390 <vms6402@gmail.com>
2023-12-14 10:57:07 +0530

    Maps Iteration

--- 9-Data-Structures-Operators/script.js
+++ 9-Data-Structures-Operators/script.js
@@ -53,8 +53,42 @@ const restaurant = {
   },
 };
 
+const question = new Map([
+  ["question", "What is the best programming language in the world?"],
+  [1, "C"],
+  [2, "Java"],
+  [3, "JavaScript"],
+  ["correct", 3],
+  [true, "Correct"],
+  [false, "Try again!"],
+]);
+
+console.log(question);
+
+// Convert object to map:
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get("question"));
+for (const [key, value] of question) {
+  if (typeof key === "number") console.log(`Answer ${key} : ${value}`);
+}
+const answer = Number(prompt("Your answer"));
+console.log(answer);
+
+const ansStr = question.get(answer === question.get("correct"));
+console.log(ansStr);
+
+// Convert Map to Array
+console.log([...question]);
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
 ///////////////
 // Maps in Js
+/*
 
 const rest = new Map();
 rest.set("name", "Classico Italiano");
@@ -90,6 +124,8 @@ console.log(rest);
 
 // console.log(rest.get([1, 2]));   // Wont work => undefined
 console.log(rest.get(arr));
+
+*/
 
 //////////////
 // Sets in Js
