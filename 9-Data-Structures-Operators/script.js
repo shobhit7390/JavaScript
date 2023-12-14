056edf16ca2aac496da4de11922f5dccf8d64fff
shobhit7390 <vms6402@gmail.com>
2023-12-14 19:06:53 +0530

    Working with strings Part-3

--- 9-Data-Structures-Operators/script.js
+++ 9-Data-Structures-Operators/script.js
@@ -53,6 +53,63 @@ const restaurant = {
   },
 };
 
+//////////////////////////////////
+// Working with Strings- Part 3
+
+// split and join
+console.log("a+very+nice+song".split("+"));
+console.log("Shobhit Yadav".split(" "));
+
+const [firstName, lastName] = "Shobhit Yadav".split(" ");
+const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(" ");
+  const namesUpper = [];
+
+  for (const n of names) {
+    namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    // namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(" "));
+};
+
+capitalizeName("jessica and smith david");
+capitalizeName("shobhit yadav");
+
+// Padding
+const message = "Go to gate 23!";
+console.log(message.padStart(20, "+").padEnd(30, "+"));
+console.log("Shobhit".padStart(20, "+").padEnd(30, "+"));
+
+const maskCreditCard = function (number) {
+  const str = number + "";
+  const last = str.slice(-4);
+  return last.padStart(str.length, "*");
+};
+
+console.log(maskCreditCard(2763787972));
+console.log(maskCreditCard(27637879728713));
+console.log(maskCreditCard("996398792792871"));
+
+// Repeat Method
+
+const message2 = "Bad Weather... All departures delayed... ";
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${">->- ".repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+/*
+//////////////////////////////////
+// Working with Strings- Part 2
+
 const airline = "TAP Air Portugal";
 
 console.log(airline.toLowerCase());
@@ -110,6 +167,8 @@ const checkBaggage = function (items) {
 checkBaggage("I have a laptop,some Food and a pocket Knife");
 checkBaggage("Socks and camera");
 checkBaggage("Get some snacks and a gun for protection");
+
+*/
 
 /*
 
