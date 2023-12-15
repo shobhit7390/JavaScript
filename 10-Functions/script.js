3fc6fcd655f7324bfe1a3abaafd5f8eb8ed4bd0b
shobhit7390 <vms6402@gmail.com>
2023-12-15 20:03:32 +0530

    The bind method

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -124,6 +124,9 @@ greetArr("Welcome")("Shobhit");
 
 */
 
+///////////////////////////////////
+// The call and apply methods
+
 const lufthansa = {
   airline: "Lufthansa",
   iataCode: "LH",
@@ -173,3 +176,52 @@ book.apply(swiss, flightData);
 
 console.log(swiss);
 book.call(swiss, ...flightData);
+
+///////////////////////////
+// The bind method :
+
+// book.call(eurowings, 23, "Sarah");
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookSAL = book.bind(swiss);
+
+bookEW(18, "Virat Kohli");
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23("Mack");
+bookEW23("Marco");
+
+// With event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+// lufthansa.buyPlane();
+
+document
+  .querySelector(".buy")
+  .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+
+// Partial Application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = (value) => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(5500));
+
+// Challenge: Implementing addVAT using fn returning fn.
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    console.log(value + value * rate);
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+addVAT2(5500);
+addVAT2(100);
