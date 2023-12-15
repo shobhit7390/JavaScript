2a66870033729cf26bf018905f367f38af2e6356
shobhit7390 <vms6402@gmail.com>
2023-12-15 16:44:45 +0530

    Working of passing arguments

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+/*
+//////////////////////////
+// Default parameters:
+
 const bookings = [];
 
 const createBooking = function (
@@ -25,3 +29,40 @@ createBooking("IND77", 2, 1000);
 createBooking("LH123", 2);
 createBooking("LH123", 5);
 createBooking("LH123", undefined, 990);
+
+*/
+
+//////////////////////////////////////////
+// Passing arguments Value vs Reference
+
+const flight = "IND773";
+const shobhit = {
+  name: "Shobhit Yadav",
+  passport: 81670204597,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "IND934";
+  passenger.name = "Mr. " + passenger.name;
+
+  if (passenger.passport === 81670204597) {
+    alert("Checked In");
+  } else {
+    alert("Wrong Passport!");
+  }
+};
+
+// checkIn(flight, shobhit);
+// console.log(flight);
+// console.log(shobhit);
+
+// Is the same doing....
+// const flightNum = flight;
+// const passenger = shobhit;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() + 10000000000);
+};
+
+newPassport(shobhit);
+checkIn(flight, shobhit);
