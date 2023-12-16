ab6416aca2a527ee67f400ab50310b7eebe2c1dd
shobhit7390 <vms6402@gmail.com>
2023-12-16 21:35:35 +0530

    The new at method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -75,6 +75,9 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+/*
+////////////////////////////
+// Simple array methods
 let arr = ["a", "b", "c", "d", "e"];
 
 // Slice
@@ -107,3 +110,20 @@ console.log([...arr, ...arr2]);
 
 // Join
 console.log(letters.join(" - "));
+
+*/
+
+///////////////////////////////
+// The new at method
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log("jonas".at(0));
+console.log("jonas".at(-1));
