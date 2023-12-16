f7746680aef8960830679a8f63ddefe2a2d65f66
shobhit7390 <vms6402@gmail.com>
2023-12-16 21:56:03 +0530

    Looping arrays using forEach

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ["GBP", "Pound sterling"],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -113,6 +113,7 @@ console.log(letters.join(" - "));
 
 */
 
+/*
 ///////////////////////////////
 // The new at method
 
@@ -127,3 +128,31 @@ console.log(arr.at(-1));
 
 console.log("jonas".at(0));
 console.log("jonas".at(-1));
+*/
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log("----- foreach -------");
+
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+// *** You cant use break and continue in forEach.
+
+// 0:function(200)
+// 0:function(450)
+// 0:function(400)
+// ....
