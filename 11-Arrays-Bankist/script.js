90bcf081af36c6c8166d03e36d0a19c4dac3f454
shobhit7390 <vms6402@gmail.com>
2023-12-17 19:54:32 +0530

    The filter method

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -98,7 +98,7 @@ console.log(accounts);
 /////////////////////////////////////////////////
 // LECTURES
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -288,3 +288,20 @@ const movementsDescriptions = movements.map(
 console.log(movementsDescriptions);
 
 */
+
+////////////////////////////
+// The filter method:
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter((mov) => mov < 0);
+console.log(withdrawals);
