94a49a39977003425007f9e13d9e419a7b955585
shobhit7390 <vms6402@gmail.com>
2023-12-18 20:37:15 +0530

    More ways of creating and filling arrays

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -644,6 +644,7 @@ console.log(overallBalance2);
 
 //////////////////////////
 // Sorting Arrays:
+/*
 
 // Strings
 const owners = ["Jonas", "Zach", "Adam", "Martha"];
@@ -673,3 +674,44 @@ console.log(movements);
 // });
 movements.sort((a, b) => b - a);
 console.log(movements);
+
+*/
+
+////////////////////////////////////////////////
+// More ways of creating and filling arrays:
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Empty arrays+fill method
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+// x.fill(1);
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+// Assignment
+// const dice = Array.from({ length: 100 }, () =>
+//   Math.trunc(Math.random() * 6 + 1)
+// );
+// console.log(dice);
+
+labelBalance.addEventListener("click", function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll(".movements__value"),
+    (el) => Number(el.textContent.replace("€", ""))
+  );
+
+  console.log(movementsUI);
+});
