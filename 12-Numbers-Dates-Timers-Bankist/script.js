0ec2655e3b8f537b70466d527e23fbd4fa470d83
shobhit7390 <vms6402@gmail.com>
2023-12-19 20:16:13 +0530

    The remainder operator

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -290,7 +290,7 @@ console.log(Number.isInteger(23 / 0));
 
 //////////////////////////////////
 // Math and Rounding
-
+/*
 console.log(Math.sqrt(25));
 console.log(25 ** (1 / 2));
 console.log(8 ** (1 / 3));
@@ -331,3 +331,26 @@ console.log((2.7).toFixed(0)); // toFixed() Returns string
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2)); // this returns integer
+
+*/
+
+///////////////////////////////
+// The remainder operator
+
+console.log(5 % 2);
+console.log(5 / 2);
+
+console.log(8 % 3);
+console.log(8 / 3);
+
+const isEven = (n) => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(87));
+console.log(isEven(264));
+
+labelBalance.addEventListener("click", function () {
+  [...document.querySelectorAll(".movements__row")].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = "orangered";
+    if (i % 3 === 0) row.style.backgroundColor = "blue";
+  });
+});
