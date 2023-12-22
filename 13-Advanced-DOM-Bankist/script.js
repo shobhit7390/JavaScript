886f4a8e7c1cefb8fc57ddbdeb0690a65b408e47
shobhit7390 <vms6402@gmail.com>
2023-12-22 22:19:01 +0530

    Building a tabbed component

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -91,6 +91,30 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
   }
 });
 
+// Tabbed Component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  // Gaurd clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach((t) => t.classList.remove("operations__tab--active"));
+  tabsContent.forEach((c) => c.classList.remove("operations__content--active"));
+
+  // Activate tab
+  clicked.classList.add("operations__tab--active");
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add("operations__content--active");
+});
+
 //////////////////////////////////////////
 //////////////////////////////////////////
 /*
@@ -238,7 +262,7 @@ document.querySelector(".nav").addEventListener("click", function (e) {
 
 ///////////////////////////////////////////
 // DOM Traversing
-
+/*
 const h1 = document.querySelector("h1");
 
 // Going downwards
@@ -267,3 +291,5 @@ console.log(h1.parentElement.children);
 [...h1.parentElement.children].forEach(function (el) {
   if (el !== h1) el.style.transform = "scale(0.5)";
 });
+
+*/
