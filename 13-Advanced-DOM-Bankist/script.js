f1cca8a23f89e283f1ca508ad1237510edeaa5f0
shobhit7390 <vms6402@gmail.com>
2023-12-23 19:39:23 +0530

    The intersection observer API

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -142,6 +142,54 @@ nav.addEventListener("mouseout", handleHover.bind(1));
 //   handleHover(e, 1);
 // });
 
+// Sticky navigation
+// const initialCoords = section1.getBoundingClientRect();
+// // console.log(initialCoords);
+
+// window.addEventListener("scroll", function () {
+//   // console.log(window.scrollY);
+
+//   if (window.scrollY > initialCoords.top) nav.classList.add("sticky");
+//   else nav.classList.remove("sticky");
+// });
+
+//////////////////////////////////////////////////
+// Sticky navigation : Intersection observer API
+
+// const obsCallback = function (entries, observer) {
+//   entries.forEach((entry) => {
+//     console.log(entry);
+//   });
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: 0.1,
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
+
 //////////////////////////////////////////
 //////////////////////////////////////////
 /*
