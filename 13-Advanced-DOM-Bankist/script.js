5026979651a30b124e1a7b87c511a346a5a7c134
shobhit7390 <vms6402@gmail.com>
2023-12-26 22:14:32 +0530

    Lifecycle DOM events

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -505,3 +505,20 @@ console.log(h1.parentElement.children);
 });
 
 */
+
+/////////////////////////////////////////////
+// Lifecycle DOM events
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  console.log("HTML parsed and DOM tree built!", e);
+});
+
+window.addEventListener("load", function (e) {
+  console.log("Page fully loaded");
+});
+
+// window.addEventListener("beforeunload", function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = "";
+// });
